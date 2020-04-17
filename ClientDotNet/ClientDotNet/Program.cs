ec75054f8cf788a52e62fe8e389e9258293c56d4
Boris <anas.essaghir@gmail.com>
2020-04-17 17:15:50 +0200

    Add client GraphQL .NET
    (cherry picked from commit 824c86c40386a9ce8fd13235eac1e628a3b59124)

---
+++ ClientDotNet/ClientDotNet/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClientDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new GraphQLClient("http://localhost:4000");
+            var query = @"
+    query { 
+        houses{id name words}
+    }
+";
+
+            var delete = @"
+   mutation deleteHouse($id: ID!) {deleteHouse(id: $id){id name words}}
+";
+
+            var getHouse = client.Query(query, null).Get("houses");
+            if (getHouse != null)
+            {
+                foreach (var house in getHouse)
+                {
+                    Console.WriteLine($"id : ${house.id}, name : ${house.name}, words : ${house.words}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Null :(");
+            }
+
+            var deleteHouse = client.Query(delete, new { id = 5 }).Get("deleteHouse");
+            if (deleteHouse != null)
+            {
+                foreach (var house in deleteHouse)
+                {
+                    Console.WriteLine($"id : ${house.id}, name : ${house.name}, words : ${house.words}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Null :(");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
